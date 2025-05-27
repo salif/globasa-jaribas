b461e772296fa642cfae02117948f40009cd7fc4
Salif Mehmed <mail@salif.eu>
2025-05-27 23:55:13 +0300

    Add files

---
+++ savedict.js
@@ -0,0 +1,28 @@
+const fs = require("fs")
+const eng = JSON.parse(fs.readFileSync("/tmp/standard-eng.json", "utf-8"))
+const spa = JSON.parse(fs.readFileSync("/tmp/standard-spa.json", "utf-8"))
+const bul = JSON.parse(fs.readFileSync("/tmp/standard-bul.json", "utf-8"))
+const dict = { glb: { eng: {}, spa: {}, bul: {} }, eng: { glb: {} }, spa: { glb: {} }, bul: { glb: {} } }
+const f = (word, lang) => {
+	if (word.term.startsWith("-")) { return }
+	if (word.term.startsWith("(fe) ")) { word.term = word.term.substring(5) }
+	if (!dict.glb[lang].hasOwnProperty(word.term)) {
+		dict.glb[lang][word.term] = {}
+	}
+	dict.glb[lang][word.term] = word.trans[lang].flat(1)
+	word.trans[lang].forEach(meanings => {
+		meanings.forEach(meaning => {
+			if (meaning.startsWith("-")) { return }
+			if (!dict[lang].glb.hasOwnProperty(meaning)) {
+				dict[lang].glb[meaning] = []
+			}
+			if (dict[lang].glb[meaning].indexOf(word.term) === -1) {
+				dict[lang].glb[meaning].push(word.term)
+			}
+		})
+	})
+}
+Object.values(eng).forEach(e => { f(e, "eng") })
+Object.values(spa).forEach(e => { f(e, "spa") })
+Object.values(bul).forEach(e => { f(e, "bul") })
+fs.writeFileSync("dict.js", "var dict=" + JSON.stringify(dict))
